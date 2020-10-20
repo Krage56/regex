85c2a1daa63d1c9320947a3ec4612a7b3a006c45
Krage56 <ivan.proskuryakov.2001@yandex.ru>
2020-10-20 12:35:57 +0300

    Beautified code with arguments from SELECT & FROM

--- main.cpp
+++ main.cpp
@@ -2,80 +2,63 @@
 #include "fstream"
 #include "regex"
 using namespace std;
-int main()
-{
-    ifstream file("input.txt");
-    string command(R"(SELECT * from trash_table.csv)");
-    string command1("SELECT res1, test2,test3,test4 from, trash_table.csg");
-    //getline(cin, command);
-    //cmatch res;
-    smatch res1{};
-    regex select_expr(R"((^SELECT)\s+([\w_,\s]+)\s+([^(from)]*)\s+)", regex::icase);
+
+[[nodiscard]] vector<string> selection_args(const string& command){
+    vector<string>result;
+    regex select_exp(R"((^SELECT)\s+([\w_,\s]+)\s+([^(from)]*)\s+)", regex::icase);
     regex select_spec_exp(R"((^SELECT)\s+([*\s]))", regex::icase);
-    regex from_expr(R"((from)\s+([\w_.csv]+))", regex::icase);
-    regex rex(R"((^SELECT)\s+([\w_,\s]+)\s+(from)\s+([\w_.csv]+))", regex::icase);
-    regex rex1("([\\w_]+)");
-    vector<string> array;
-    regex_search(command, res1, rex);
-    for(const auto& el: res1){
-        cout << el << endl;
-    }
-    cout << "___________________" << endl;
-    smatch res2{}, res3{};
-    regex_search(command, res2, select_expr);
-    regex_search(command, res3, from_expr);
-    if(res2.size()){
-//        for(const auto& el: res2){
-//            cout << el << endl;
-//        }
-        auto iter = res2.begin() + 2;
-        for(iter; iter != res2.end(); ++iter){
+    regex columns_exp("([\\w_]+)");
+    smatch matches{};
+    regex_search(command, matches, select_exp);
+    const int delta = 2;
+    if(!matches.empty()){
+        auto iter = matches.begin() + delta;
+        for(iter; iter != matches.end(); ++iter){
             smatch tmp{};
             string tmp_str(iter->str());
-            //cout << iter->str() << endl;
             auto begin = tmp_str.cbegin();
             auto end = tmp_str.cend();
-            while(regex_search(begin, end, tmp, rex1))
+            while(regex_search(begin, end, tmp, columns_exp))
             {
-                array.push_back(tmp.str());
-                cout << tmp.str() << endl;
+                result.push_back(tmp.str());
                 begin += tmp.position() + tmp.str().size();
             }
         }
     }
     else{
-        regex_search(command, res2, select_spec_exp);
-        if(res2.size()){
-            cout << "*" << endl;
+        regex_search(command, matches, select_spec_exp);
+        if(!matches.empty()){
+            result.emplace_back("*");
         }
     }
+    return result;
+}
+[[nodiscard]] vector<string> from_args(const string& command){
+    regex from_expr(R"((from)\s+([\w_.csv]+))", regex::icase);
+    smatch matches;
+    regex_search(command, matches, from_expr);
+    vector<string> result;
+    const int delta = 2;
+    auto iter = matches.cbegin() + delta;
+    for(; iter != matches.cend(); ++iter){
+        result.push_back(iter->str());
+    }
+    return result;
+}
+int main()
+{
+    ifstream file("input.txt");
+    string command(R"(SELECT * from trash_table.csv)");
+    string command1("SELECT res1, test2,test3,test4 from trash_table.csg");
 
-    cout << "___________________" << endl;
-    for(const auto& el: res3){
+    auto selection_arguments = move(selection_args(command));
+    for(const auto& el: selection_arguments){
         cout << el << endl;
     }
     cout << "___________________" << endl;
-//    regex_search(command.c_str(), res, rex);
-//    if (res.empty())
-//        throw std::runtime_error("notkek");
-//    string s1(res[2].str());
-    //regex_search(s1.c_str(), res1, rex1);
-//    auto begin = s1.cbegin();
-//    auto end = s1.cend();
-//    while(regex_search(begin, end, res1, rex1))
-//    {
-//        array.push_back(res1.str());
-//        begin += res1.position() + res1.str().size();
-//    }
-//    if (res1.empty())
-//        throw std::runtime_error("notkek1");
-//    for (const auto& el: res1)
-//    {
-//
-//        cout << el<<endl;
-//    }
-//        cout << array[0] << endl;
-//        cout << array[1] << endl;
-//        cout << array[2];
+    auto from_argument = move(from_args(command));
+    for(const auto& el: from_argument){
+        cout << el << endl;
+    }
 
 }
